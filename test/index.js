c89e231c18d2911489dfc07e6394494202f6bd91
blanca <978059629@qq.com>
2022-04-19 00:54:43 +0800

    删除test文件夹

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const read = fs.createReadStream("./11.txt", { encoding: "utf-8" });
-const write = fs.createWriteStream("./22.txt");
-read.push('123')
-read.on("data", (chunk) => {
-  console.log(chunk);
-});
-read.push('444')
