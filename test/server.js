7771b36f79be1cbcf928b7c7c4aced46c2c46b5c
blanca <978059629@qq.com>
2022-04-18 15:35:23 +0800

    合并

--- test/server.js
+++ test/server.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-app.use(express.static(__dirname));
-app.get("/getdata", (req, res) => {
-  res.setHeader("Set-Cookie", "server=server777;Max-Age=10;HttpOnly");
-  res.setHeader("Cache-Control", "no-store");
-  console.log(req.headers.cookie)
-  res.json({
-    name: "blanca",
-  });
+app.get("/api/getdata", (req, res) => {
+  console.log('有人调用getdata')
+  res.setHeader('name','blanca')
+  res.json({ name: "blanca" }).end();
 });
 app.listen(3000, () => {
   console.log("open in 3000");
